9b5310fd42643d9290d3fa864479fe2c02309a0b
Gulzat Karimova <gulzat.karimova@gmail.com>
2019-01-11 16:31:55 -0800

    It will change the file to execute tests successfully.

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -10,26 +10,25 @@ describe("Galactic age calculator", function() {
   });
 
   it("should calculate user's age in Mercury years.", function() {
-    expect(inputOne.mercuryAge).toEqual(inputOne.age/0.24);
+    expect(inputOne.mercuryAge()).toEqual(Math.floor(inputOne.age/0.24));
   });
 
   it("should calculate user's age in Venus years.", function() {
-    expect(inputOne.venusAge).toEqual(inputOne.age/0.62);
+    expect(inputOne.venusAge()).toEqual(Math.floor(inputOne.age/0.62));
   });
 
   it("should calculate user's age in Mars years.", function() {
-    expect(inputOne.marsAge).toEqual(inputOne.age/1.88);
+    expect(inputOne.marsAge()).toEqual(Math.floor(inputOne.age/1.88));
   });
 
   it("should calculate user's age in Jupiter years.", function() {
-    expect(inputOne.jupiterAge).toEqual(inputOne.age/11.86);
+    expect(inputOne.jupiterAge()).toEqual(Math.floor(inputOne.age/11.86));
   });
 
   it("should determines how many years a user has left to live on each planet based on average life expectency.", function() {
-    expect(inputOne.mercuryLeftYears).toEqual(78/0.24-inputOne.mercuryAge);
-    expect(inputOne.venusLeftYears).toEqual(78/0.62-inputOne.venusAge);
-    expect(inputOne.marsLeftYears).toEqual(78/1.88-inputOne.marsAge);
-    expect(inputOne.jupiterLeftYears).toEqual(78/11.86-inputOne.jupiterAge);
-
+    expect(inputOne.mercuryLeftYears()).toEqual(Math.floor(78/0.24-inputOne.mercuryAge()));
+    expect(inputOne.venusLeftYears()).toEqual(Math.floor(78/0.62-inputOne.venusAge()));
+    expect(inputOne.marsLeftYears()).toEqual(Math.floor(78/1.88-inputOne.marsAge()));
+    expect(inputOne.jupiterLeftYears()).toEqual(Math.floor(78/11.86-inputOne.jupiterAge()));
   });
 });
